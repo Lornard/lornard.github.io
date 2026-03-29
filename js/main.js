24373d275f7a485fbc9ef670fab2c0d9fdf2a477
Lornard <gabriel.silveira@gmail.com>
2026-03-29 20:51:36 -0300

    Tentando passar o inicio do carrossel para o onload quando os elementos já estão carregados.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-const myCarouselElement = document.getElementById('carroselEntrada')
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 2000,
-  wrap: false
-})
+function iniciarCarrossel()
+{
+  const myCarouselElement = document.getElementById('carroselEntrada')
+  const carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: 2000,
+    wrap: false
+  })
+}
