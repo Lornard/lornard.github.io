03720142700e4c7a927d40659d3932a6d5b3ce04
Lornard <gabriel.silveira@gmail.com>
2026-03-30 13:58:46 -0300

    Ajusta código javascript para comparação.

--- js/servicos.js
+++ js/servicos.js
@@ -1,5 +1,5 @@
 function confirmarAgendamento(dateElement){
-    var data = dateElement.value.toISOString();
+    var data = dateElement.value;
     var agora = new Date().toISOString();
     if (data < agora) {
         alert('Selectione uma data válida');
